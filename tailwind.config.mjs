68351805b741739bc2e8abacf74a479c67afe9e4
Emad Kazemi <emad@pro.ai>
2024-09-09 20:00:20 +0330

    landing is half done

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,9 +4,24 @@ module.exports = {
 		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
 		'./node_modules/flowbite/**/*.js'
 	],
-	theme: {
-		extend: {},
-	},
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: '#fbe9f5',
+          100: '#f4bde0',
+          200: '#ed91cb',
+          300: '#e665b6',
+          400: '#df3aa2',
+          500: '#c52088',
+          600: '#9a196a',
+          700: '#6e124c',
+          800: '#420b2d',
+          900: '#16040f',
+        },
+      },
+    },
+  },
 	plugins: [
 		require('flowbite/plugin')
 	],
